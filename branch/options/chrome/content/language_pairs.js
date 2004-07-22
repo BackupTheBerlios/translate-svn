5c8599d4d4bc926e7a120fd9adf27e1e02ac1243
ctomer <ctomer@65cb7af9-c95a-c347-8150-f05b536030d3>
2004-07-22 18:22:09 +0000

    debugging: emptied language pairs

--- branch/options/chrome/content/language_pairs.js
+++ branch/options/chrome/content/language_pairs.js
@@ -1,15 +1,2 @@
-var languagePairs = new Array();
-languagePairs[0] = new Array("en","fr", "de", "es", "it","nl", "pt", "el", "ko","ja", "zh", "zt", "ru");
-languagePairs[1] = new Array("fr","en", "de", "es", "it","pl", "nl", "el");
-languagePairs[2] =  new Array("de","en", "fr");
-languagePairs[3] =  new Array("es","en", "fr");
- languagePairs[4] =  new Array("it","en", "fr");
- languagePairs[5] =  new Array("el","en", "fr");
- languagePairs[6] =  new Array("ko","en");
- languagePairs[7] =  new Array("ja","en");
- languagePairs[8] =  new Array("zh","en");
- languagePairs[9] =  new Array("zt","en");
- languagePairs[10] =  new Array("nl","en", "fr");
- languagePairs[11] =  new Array("ru","en");
- languagePairs[12] =  new Array("pt","en", "fr");
+
 
