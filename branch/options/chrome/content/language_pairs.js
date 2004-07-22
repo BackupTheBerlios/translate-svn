19ae9003ee4d056560c5b8d8a4f3396d50f327f7
ctomer <ctomer@65cb7af9-c95a-c347-8150-f05b536030d3>
2004-07-22 16:21:48 +0000

    initial import

---
+++ branch/options/chrome/content/language_pairs.js
@@ -0,0 +1,23 @@
+function lnaguagePairs(toLang, fromLang) {
+   this.toLang = toLang;
+   this.fromLang = fromLang;
+   
+}
+
+
+
+var en = ["fr", "de", "es", "it","nl", "pt", "el", "ko","ja", "zh", "zt", "ru"]
+var fr = ["en", "de", "es", "it","pl", "nl", "el"]
+var de = ["en", "fr"]
+var es = ["en", "fr"]
+var it = ["en", "fr"]
+var el = ["en", "fr"]
+var ko = ["en"]
+var ja = ["en"]
+var zh = ["en"]
+var zt = ["en"]
+var nl = ["en", "fr"]
+var ru = ["en"]
+var pt = ["en", "fr"]
+
+
