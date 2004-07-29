090ddfa8124323addbb92e31fdddc7b4466f5803
(no author) <(no author)@65cb7af9-c95a-c347-8150-f05b536030d3>
2004-07-29 19:24:02 +0000

    update unicode to fix chinese

--- trunk/chrome/content/language_pairs.js
+++ trunk/chrome/content/language_pairs.js
@@ -17,13 +17,29 @@ const glanguageUnicode = new Array();
 glanguageUnicode[0] = "English";
 glanguageUnicode[1] = "Fran\u00E7ais";
 glanguageUnicode[2] =  "Deutsch";
+<<<<<<< .mine
+glanguageUnicode[3] =  "espa\u00F1ol";
+=======
 glanguageUnicode[3] =  "Espa\u00F1ol;
+>>>>>>> .r80
 glanguageUnicode[4] =  "italiano";
+<<<<<<< .mine
+glanguageUnicode[5] =  "\u03B5\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC";
+glanguageUnicode[6] =  "\uD55C\uAD6D\uC5B4";
+glanguageUnicode[7] =  "\u65E5\u672C\u8A9E";
+glanguageUnicode[8] = "\u7B80\u4F53\u4E2D\u6587";
+glanguageUnicode[9] =  "\u7E41\u9AD4\u4E2D\u6587";
+=======
 glanguageUnicode[5] =  "\u03B5\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC";  //greek
 glanguageUnicode[6] =  "\uD55C\uAD6D\uC5B4";  // korean
 glanguageUnicode[7] =  "\u65E5\u672C\u8A9E";  // japanese
 glanguageUnicode[8] = "\u7B80\u4F53\u4E2D\u6587"; // chinese simple
 glanguageUnicode[9] =  "\u7E41\u9AD4\u4E2D\u6587"; // chinese trad
+>>>>>>> .r80
 glanguageUnicode[10] =  "Nederlands";
+<<<<<<< .mine
+glanguageUnicode[11] =  "\u0440\u0443\u0441\u0441\u043A\u0438\u0439";
+glanguageUnicode[12] =  "portugu\u00EAs";
+=======
 glanguageUnicode[11] =  "\u0440\u0443\u0441\u0441\u043A\u0438\u0439";  //russian
-glanguageUnicode[12] =  "portugu\u00EAs
+glanguageUnicode[12] =  "portugu\u00EAs>>>>>>> .r80
