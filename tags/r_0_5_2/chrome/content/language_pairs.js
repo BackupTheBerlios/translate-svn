d36b489f99f5557cbbcf7b907005583c59dd4360
(no author) <(no author)@65cb7af9-c95a-c347-8150-f05b536030d3>
2004-07-29 19:40:56 +0000

    release 0.5.2

---
+++ tags/r_0_5_2/chrome/content/language_pairs.js
@@ -0,0 +1,45 @@
+const glanguagePairs = new Array();
+glanguagePairs[0] = new Array("en","fr", "de", "es", "it","nl", "pt", "el", "ko","ja", "zh", "zt", "ru");
+glanguagePairs[1] = new Array("fr","en", "de", "es", "it","pt", "nl", "el");
+glanguagePairs[2] =  new Array("de","en", "fr");
+glanguagePairs[3] =  new Array("es","en", "fr");
+glanguagePairs[4] =  new Array("it","en", "fr");
+glanguagePairs[5] =  new Array("el","en", "fr");
+glanguagePairs[6] =  new Array("ko","en");
+glanguagePairs[7] =  new Array("ja","en");
+glanguagePairs[8] =  new Array("zh","en");
+glanguagePairs[9] =  new Array("zt","en");
+glanguagePairs[10] =  new Array("nl","en", "fr");
+glanguagePairs[11] =  new Array("ru","en");
+glanguagePairs[12] =  new Array("pt","en", "fr");
+
+const glanguageUnicode = new Array();
+glanguageUnicode[0] = "English";
+glanguageUnicode[1] = "Fran\u00E7ais";
+glanguageUnicode[2] =  "Deutsch";
+<<<<<<< .mine
+glanguageUnicode[3] =  "espa\u00F1ol";
+=======
+glanguageUnicode[3] =  "Espa\u00F1ol;
+>>>>>>> .r80
+glanguageUnicode[4] =  "italiano";
+<<<<<<< .mine
+glanguageUnicode[5] =  "\u03B5\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC";
+glanguageUnicode[6] =  "\uD55C\uAD6D\uC5B4";
+glanguageUnicode[7] =  "\u65E5\u672C\u8A9E";
+glanguageUnicode[8] = "\u7B80\u4F53\u4E2D\u6587";
+glanguageUnicode[9] =  "\u7E41\u9AD4\u4E2D\u6587";
+=======
+glanguageUnicode[5] =  "\u03B5\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC";  //greek
+glanguageUnicode[6] =  "\uD55C\uAD6D\uC5B4";  // korean
+glanguageUnicode[7] =  "\u65E5\u672C\u8A9E";  // japanese
+glanguageUnicode[8] = "\u7B80\u4F53\u4E2D\u6587"; // chinese simple
+glanguageUnicode[9] =  "\u7E41\u9AD4\u4E2D\u6587"; // chinese trad
+>>>>>>> .r80
+glanguageUnicode[10] =  "Nederlands";
+<<<<<<< .mine
+glanguageUnicode[11] =  "\u0440\u0443\u0441\u0441\u043A\u0438\u0439";
+glanguageUnicode[12] =  "portugu\u00EAs";
+=======
+glanguageUnicode[11] =  "\u0440\u0443\u0441\u0441\u043A\u0438\u0439";  //russian
+glanguageUnicode[12] =  "portugu\u00EAs>>>>>>> .r80
